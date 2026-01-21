a0adc02493d9845eab865c8dd322a49fa83fda69
Neha Singh <66407992+Neha9492@users.noreply.github.com>
2026-01-21 18:47:46 +0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -33,12 +33,12 @@
 
           if (typeof patient.name[0] !== 'undefined') {
             fname = patient.name[0].given.join(' ');
-            lname = patient.name[0].family;//.join(' ');
+            lname = patient.name[0].family;
           }
 
           var height = byCodes('8302-2');
-         // var systolicbp = getBloodPressureValue(byCodes('8480-6');
-         // var diastolicbp = getBloodPressureValue(byCodes('8462-4');
+          var systolicbp = getBloodPressureValue(byCodes((85354-9),'8480-6');
+          var diastolicbp = getBloodPressureValue(byCodes((85354-9),'8462-4');
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
 
